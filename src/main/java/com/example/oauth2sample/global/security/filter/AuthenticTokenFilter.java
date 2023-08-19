7c04f170dd9a0e234e1d5f43da050eac4506871d
mk1-p <mk1.dev662@gmail.com>
2023-08-19 14:45:21 +0900

    ENH: 로직 스켈레톤 기입

--- src/main/java/com/example/oauth2sample/global/security/filter/AuthenticTokenFilter.java
+++ src/main/java/com/example/oauth2sample/global/security/filter/AuthenticTokenFilter.java
@@ -12,10 +12,22 @@ import java.io.IOException;
 @Component
 public class AuthenticTokenFilter extends OncePerRequestFilter {
 
+    // OncePerRequestFilter 한번의 요청에 한번의 필터 검증
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+
+        // token 종류 확인
+
+        // token 토큰 유효기간 확인
+
+        // 유효기간에 따라 갱신 여부 결정
+
+        // Refresh Token도 만료 된 경우, 재 로그인 알림 또는 redirectUri 지정
+
 
         filterChain.doFilter(request, response);
 
